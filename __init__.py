e9b4379e07b6c4c56b148a75248d9016d56d7ec6
er5241 <er5241@anka-stroboscode.anka.kit.edu>
2021-11-15 14:27:24 +0100

    updated setup.py

--- __init__.py
+++
@@ -1,1 +0,0 @@
-from .univ_read import *
--- setup.py
+++ setup.py
@@ -1,19 +1,15 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
 with open('requirements.txt') as file:
     reqs = file.readlines()
     reqs = [line.rstrip() for line in lines]
 
 setuptools.setup(
-    name='univ_read',
+    name='univread',
     version='0.0.1',
     author='Jwalin Bhatt',
     author_email='jwalin.bhatt@gmail.com',
     description='Universal 3D file reader',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url='https://github.com/DL4XRayTomoImaging-KIT/UnivRead3D',
     packages=setuptools.find_packages(),
     install_requires=reqs,
