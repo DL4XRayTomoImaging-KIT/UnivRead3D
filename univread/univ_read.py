cf3a9c747aebd1f19aa07ae11cf3c41535be37ce
er5241 <er5241@anka-stroboscode.anka.kit.edu>
2021-12-07 03:13:59 +0100

    added reading multifile tiffs

--- univread/univ_read.py
+++ univread/univ_read.py
@@ -1,28 +1,39 @@
 import os
 import numpy as np
+from tqdm.auto import tqdm
 
 import nrrd
 import tifffile
 import nibabel as nib
 
+from concert.readers import TiffSequenceReader
+
 def read(file_):
-  fname, ext = os.path.splitext(file_)
+  if os.path.isdir(file_):
+    seq_reader = TiffSequenceReader(os.path.join(file_, '*.tif*'))
+    pg_0 = seq_reader.read(0)
+
+    img = np.zeros((seq_reader.num_images, *pg_0.shape), dtype=pg_0.dtype)
+    for i in tqdm(range(seq_reader.num_images)):
+      img[i] = seq_reader.read(i)
   
-  if ext == '.tif' or ext == '.tiff':
-    descriptor = tifffile.TiffFile(file_)
-    img = descriptor.asarray()
-    
-    full_len = len(descriptor.pages)
-    loaded_len = len(img)
+  else:
+    fname, ext = os.path.splitext(file_)
+  
+    if ext == '.tif' or ext == '.tiff':
+      descriptor = tifffile.TiffFile(file_)
+      img = descriptor.asarray()
+      full_len = len(descriptor.pages)
+      loaded_len = len(img)
  
-    if loaded_len < full_len:
+      if loaded_len < full_len:
         img = np.concatenate([img, np.zeros((full_len - loaded_len, *img.shape[1:]), dtype=img.dtype)])
         for i in range(loaded_len, full_len):
-            img[i] = descriptor.pages[i].asarray()
+          img[i] = descriptor.pages[i].asarray()
  
-  elif ext == '.nrrd':
-    img = nrrd.read(file_)[0]
-  elif ext == '.nii':
-    img = nib.load(file_)
+    elif ext == '.nrrd':
+      img = nrrd.read(file_)[0]
+    elif ext == '.nii':
+      img = nib.load(file_)
     
-  return img  
+  return img
