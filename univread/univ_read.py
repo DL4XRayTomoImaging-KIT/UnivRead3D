44a8ed33fb7d720797a43515b6fb724cbb859416
er5241 <er5241@anka-stroboscode.anka.kit.edu>
2022-04-19 15:46:33 +0200

    added to option to lazy read

--- univread/univ_read.py
+++ univread/univ_read.py
@@ -2,6 +2,7 @@ import os
 import warnings
 import numpy as np
 from glob import glob
+import dask.array as da
 
 import nrrd
 import tifffile
@@ -9,16 +10,24 @@ import nibabel as nib
 from medpy.io import load as medload
 
 
-def read(file_):
+def read(file_, lazy=False):
+  
   if os.path.isdir(file_):  # paginated tiffs
     fprefix = os.path.join(file_, '*.tif*')
     fnames = sorted(glob(fprefix))
     
-    file_0 = tifffile.TiffFile(fnames[0]).asarray()
-    if len(file_0.shape) == 2:
-      img = np.stack([tifffile.imread(fname) for fname in fnames])
+    if not lazy:
+      file_0 = tifffile.imread(fnames[0])
+      if len(file_0.shape) == 2:
+        img = np.stack([tifffile.imread(fname) for fname in fnames])
+      else:
+        img = np.concatenate([tifffile.imread(fname) for fname in fnames])
     else:
-      img = np.concatenate([tifffile.imread(fname) for fname in fnames])
+      file_0 = tifffile.memmap(fnames[0])
+      if len(file_0.shape) == 2:
+        img = da.stack([tifffile.memmap(fname) for fname in fnames])
+      else:
+        img = da.concatenate([tifffile.memmap(fname) for fname in fnames])
   
   else:
     name = file_.split('.')
@@ -27,20 +36,25 @@ def read(file_):
   
     if ext in ['.tif', '.tiff']:
       descriptor = tifffile.TiffFile(file_)
-      img = descriptor.asarray()
+      img = descriptor.asarray() if not lazy else tifffile.memmap(file_)
       full_len = len(descriptor.pages)
       loaded_len = len(img)
  
+      # for multi-paginated tiffs
       if loaded_len < full_len:
-        img = np.concatenate(img, np.zeros((full_len - loaded_len, *img.shape[1:])))
+        if lazy: raise Exception('Not implemented')
+        img = np.concatenate([img, np.zeros((full_len - loaded_len, *img.shape[1:]))])
         for i in range(loaded_len, full_len):
           img[i] = descriptor.pages[i].asarray()
  
     elif ext == '.nrrd':
+      if lazy: raise Exception('Not implemented')
       img = nrrd.read(file_)[0]
     elif ext in ['.nii', '.nii.gz']:
-      img = np.array(nib.load(file_).dataobj)
+      img = nib.load(file_)
+      if not lazy: img = np.array(img.dataobj)
     else:  # load with medpy as the default case
+      if lazy: raise Exception('Not implemented')
       warnings.warn('unrecognized file extension, proceeding to load with medpy')
       img = medload(file_)[0]
 
