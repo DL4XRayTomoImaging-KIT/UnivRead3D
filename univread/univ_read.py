efe4ae147cd2f18124b2413a0ddab0ce248a3535
er5241 <er5241@anka-stroboscode.anka.kit.edu>
2022-04-25 23:47:27 +0200

    restructured code

--- univread/univ_read.py
+++ univread/univ_read.py
@@ -10,70 +10,78 @@ import nibabel as nib
 from medpy.io import load as medload
 
 
+def read_tiffdir(fpath, lazy):
+  fnames = sorted(glob(f'{fpath}/*.tif*'))
+  file_0 = tifffile.memmap(fnames[0]) if lazy else tifffile.imread(fnames[0])
+
+  if not lazy:
+    if len(file_0.shape) == 2:  # dir containing 2d tiffs
+      img = np.stack([tifffile.imread(fname) for fname in fnames])
+    else:  # dir containing 3d tiffs
+      img = np.concatenate([tifffile.imread(fname) for fname in fnames])
+
+  else:
+    shapes = np.array([tifffile.memmap(fname).shape for fname in fnames])
+
+    if len(file_0.shape) == 2:  # dir containing 2d tiffs
+      final_shape = (len(shapes), *shapes[0])
+
+      with tempfile.NamedTemporaryFile() as f:
+        img = np.memmap(f.name, dtype=file_0.dtype, mode='w+', shape=final_shape)
+        for i,fname in enumerate(fnames):
+          img[i] = tifffile.memmap(fname)
+          img = np.memmap(f.name, dtype=file_0.dtype, mode='r+', shape=final_shape)
+
+    else:  # dir containing 3d tiffs
+      final_shape = (sum(shapes[:,0]), *shapes[0][1:])
+
+      with tempfile.NamedTemporaryFile() as f:
+        img = np.memmap(f.name, dtype=file_0.dtype, mode='w+', shape=final_shape)
+        indx = 0
+        for fname in fnames:
+          descriptor = tifffile.TiffFile(fname)
+          for page in descriptor.pages:
+            img[indx] = page.asarray()
+            img = np.memmap(f.name, dtype=file_0.dtype, mode='r+', shape=final_shape)
+            indx += 1
+
+  return img
+
+
+def read_tifffile(file_, lazy):
+  descriptor = tifffile.TiffFile(file_)
+  img = tifffile.memmap(file_) if lazy else tifffile.imread(file_)
+  full_len = len(descriptor.pages)
+  loaded_len = len(img)
+
+  # for handling multi-page tiffs
+  if loaded_len < full_len:
+    if not lazy:
+      img = np.array([page.asarray() for page in descriptor.pages])
+
+    else:
+      final_shape = (full_len, *img.shape[1:])
+      with tempfile.NamedTemporaryFile() as f:
+        img = np.memmap(f.name, dtype=img.dtype, mode='w+', shape=final_shape)
+        for i, page in enumerate(descriptor.pages):
+          img[i] = page.asarray()
+          img = np.memmap(f.name, dtype=img.dtype, mode='r+', shape=final_shape)
+
+  return img
+
+
 def read(file_, lazy=False):
   # file_: path of the img file to be read
   # lazy: setting to True will return memmap instead of np array
   
   # dir containing tiffs
   if os.path.isdir(file_):
-    fnames = sorted(glob(f'{file_}/*.tif*'))
-    file_0 = tifffile.memmap(fnames[0]) if lazy else tifffile.imread(fnames[0])
-    
-    if not lazy:
-      if len(file_0.shape) == 2:  # dir containing 2d tiffs
-        img = np.stack([tifffile.imread(fname) for fname in fnames])
-      else:  # dir containing 3d tiffs
-        img = np.concatenate([tifffile.imread(fname) for fname in fnames])
-
-
-    else:
-      shapes = np.array([tifffile.memmap(fname).shape for fname in fnames])
-      
-      if len(file_0.shape) == 2:  # dir containing 2d tiffs
-        final_shape = (len(shapes), *shapes[0])
-        
-        with tempfile.NamedTemporaryFile() as f:
-          img = np.memmap(f.name, dtype=file_0.dtype, mode='w+', shape=final_shape)
-          for i,fname in enumerate(fnames):
-            img[i] = tifffile.memmap(fname)
-            img = np.memmap(f.name, dtype=file_0.dtype, mode='r+', shape=final_shape)
-        
-      else:  # dir containing 3d tiffs
-        final_shape = (sum(shapes[:,0]), *shapes[0][1:])
-
-        with tempfile.NamedTemporaryFile() as f:
-          img = np.memmap(f.name, dtype=file_0.dtype, mode='w+', shape=final_shape)
-          indx = 0
-          for fname in fnames:
-            descriptor = tifffile.TiffFile(fname)
-            for page in descriptor.pages:
-              img[indx] = page.asarray()
-              img = np.memmap(f.name, dtype=file_0.dtype, mode='r+', shape=final_shape)
-              indx += 1
-
-
+    img = read_tiffdir(file_, lazy)
 
   # single files
   else:
     if file_.endswith(('.tif', '.tiff')):
-      descriptor = tifffile.TiffFile(file_)
-      img = tifffile.memmap(file_) if lazy else tifffile.imread(file_)
-      full_len = len(descriptor.pages)
-      loaded_len = len(img)
-  
-      # for handling multi-page tiffs
-      if loaded_len < full_len:
-        if not lazy:
-          img = np.array([page.asarray() for page in descriptor.pages])
-  
-        else:
-          final_shape = (full_len, *img.shape[1:])
-          with tempfile.NamedTemporaryFile() as f:
-            img = np.memmap(f.name, dtype=img.dtype, mode='w+', shape=final_shape)
-            for i, page in enumerate(descriptor.pages):
-              img[i] = page.asarray()
-              img = np.memmap(f.name, dtype=img.dtype, mode='r+', shape=final_shape)
- 
+      img = read_tifffile(file_, lazy)
     elif file_.endswith('.nrrd'):
       if lazy: raise NotImplementedError('lazy reading not implemented for .nrrd')
       img = nrrd.read(file_)[0]
